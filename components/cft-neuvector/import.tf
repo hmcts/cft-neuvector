0e4f3d96b572e627b4a22fc7e987a2806b9eac42
Tim Jacomb <timjacomb1@gmail.com>
2023-12-15 11:06:50 +0000

    Fmt

--- components/cft-neuvector/import.tf
+++ components/cft-neuvector/import.tf
@@ -1,4 +1,4 @@
 import {
-    to = module.azurekeyvault.azurerm_key_vault_access_policy.creator_access_policy
-    id = "/subscriptions/b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb/resourceGroups/cft-neuvector-sbox-rg/providers/Microsoft.KeyVault/vaults/cftneuvector-sbox/objectId/173fde48-bd6b-44f1-bb63-6fe6c90424ca"
+  to = module.azurekeyvault.azurerm_key_vault_access_policy.creator_access_policy
+  id = "/subscriptions/b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb/resourceGroups/cft-neuvector-sbox-rg/providers/Microsoft.KeyVault/vaults/cftneuvector-sbox/objectId/173fde48-bd6b-44f1-bb63-6fe6c90424ca"
 }
